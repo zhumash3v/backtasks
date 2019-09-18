f221228c5469cc5616284746e0370deb844eedbf
zhumash3v <42340798+zhumash3v@users.noreply.github.com>
2019-09-18 19:02:41 +0600

    Create task2.php

---
+++ task2.php
@@ -0,0 +1,40 @@
+<html>
+<head>
+<title>HOMO1</title>
+</head>
+<body>
+<table style="">
+<caption>Info</caption>
+<tr>
+<th>ID</th>
+<th>Name</th>
+<th>Surname</th>
+<th>Email</th>
+
+</tr>
+<?php
+$conn = mysqli_connect("localhost", "root", "root", "test");
+// Check connection
+if ($conn->connect_error) {
+die("Connection failed: " . $conn->connect_error);
+}
+$sql = "SELECT id, name, surname,email FROM test";
+$result = $conn->query($sql);
+if ($result->num_rows > 0) {
+// output data of each row
+while ($row = $result->fetch_assoc()) {
+echo "<tr><td>" . $row["id"] . "</td><td>" . $row["name"] . "</td><td>"
+. $row["surname"] . "</td><td>" . $row["email"] . "</td><td><p><button>Delete</button></td></tr>";
+}
+echo "</table>";
+} else {
+echo "0 results";
+}
+$conn->close();
+?>
+
+</table>
+
+</body>
+
+</html>
