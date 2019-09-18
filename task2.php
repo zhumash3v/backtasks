7540766c3a9f5b6a1c281cc2e13a7b38171d98d2
zhumash3v <42340798+zhumash3v@users.noreply.github.com>
2019-09-18 19:28:31 +0600

    Update task2.php

--- task2.php
+++ task2.php
@@ -1,6 +1,6 @@
 <html>
 <head>
-<title>HOMO1</title>
+<title>task2</title>
 </head>
 <body>
 <table style="">
@@ -14,14 +14,10 @@
 </tr>
 <?php
 $conn = mysqli_connect("localhost", "root", "root", "test");
-// Check connection
-if ($conn->connect_error) {
-die("Connection failed: " . $conn->connect_error);
-}
+if ($conn->connect_error) { die("Connection failed: " . $conn->connect_error); }
 $sql = "SELECT id, name, surname,email FROM test";
 $result = $conn->query($sql);
 if ($result->num_rows > 0) {
-// output data of each row
 while ($row = $result->fetch_assoc()) {
 echo "<tr><td>" . $row["id"] . "</td><td>" . $row["name"] . "</td><td>"
 . $row["surname"] . "</td><td>" . $row["email"] . "</td><td><p><button>Delete</button></td></tr>";
